1346190378bd6c77d0b399e59148a00da0e1dfbd
Leo Zhang <leotheasianlion@gmail.com>
2022-06-13 10:32:28 -0400

    update

---
+++ cron_shell_scripts/cron_9-17.sh
@@ -0,0 +1,7 @@
+# begin cron_9-17.sh
+python_abs_path=''
+$python_abs_path /home/leo_zhang/Documents/GitHub/NewsScraper/NewsScraper.py
+$python_abs_path /home/leo_zhang/Documents/GitHub/GetDailyPic/get_daily_pic.py
+$python_abs_path /home/leo_zhang/Documents/GitHub/FlightScraper/FlightScraper.py
+# end cron_9-17.sh
+
