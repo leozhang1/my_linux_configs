45dbbf37608800cb569c9023c7437a7be47d75c4
Leo Zhang <leotheasianlion@gmail.com>
2022-06-12 22:04:53 -0400

    update

--- other/github_projects_cloning/dummy.sh
+++ other/github_projects_cloning/dummy.sh
@@ -1,26 +1,27 @@
-cd /home/leo_zhang/Documents/GitHub &&
+cd /home/leo_zhang/Documents/GitHub && \
+git clone git@github.com:leozhang1/AmazonPriceTrackerTool.git && \
+git clone git@github.com:leozhang1/ARHW1.git && \
+git clone git@github.com:leozhang1/bmbl_bot.git && \
 git clone git@github.com:leozhang1/tdr_bot.git && \
-git clone git@github.com:leozhang1/okcupidbot.git && \
-git clone git@github.com:leozhang1/PongGame.git && \
-git clone git@github.com:leozhang1/Zoom-Automation-Python.git && \
-git clone git@github.com:leozhang1/PersonalPortfolioFlutterWeb.git && \
-git clone git@github.com:leozhang1/GeneralDB.git && \
-git clone git@github.com:leozhang1/GithubAutomation.git  && \
-git clone git@github.com:leozhang1/easy-apply-linkedin.git  && \
 git clone git@github.com:leozhang1/DroneFun.git && \
+git clone git@github.com:leozhang1/easy-apply-linkedin.git  && \
+git clone git@github.com:leozhang1/FileManagementTool.git && \
+git clone git@github.com:leozhang1/FlightScraper.git && \
+git clone git@github.com:leozhang1/GeneralDB.git && \
+git clone git@github.com:leozhang1/GetDailyPic.git && \
+git clone git@github.com:leozhang1/GithubAutomation.git  && \
 git clone git@github.com:leozhang1/LeoZhang.git && \
-git clone git@github.com:leozhang1/PasswordManagerNodeJs.git && \
+# git clone git@github.com:leozhang1/my_linux_configs.git && \
 git clone git@github.com:leozhang1/nasa_daily_pic_collection.git  && \
+git clone git@github.com:leozhang1/NewsScraper.git && \
+git clone git@github.com:leozhang1/okcupidbot.git && \
+git clone git@github.com:leozhang1/PasswordManagerNodeJs.git && \
+git clone git@github.com:leozhang1/PersonalPortfolioFlutterWeb.git && \
+git clone git@github.com:leozhang1/PongGame.git && \
+git clone git@github.com:leozhang1/practice_interview_problems.git && \
+git clone git@github.com:leozhang1/summer_2022_class.git && \
+git clone git@github.com:leozhang1/tdr_bot.git && \
 git clone git@github.com:leozhang1/TruistCreditCardManager.git
-
-
-
-
-
-
-
-
-
-
+git clone git@github.com:leozhang1/Zoom-Automation-Python.git && \
 
 
