4409651d18b8647d88f754e8f4636d8ac0d2ed87
Leo Zhang <leotheasianlion@gmail.com>
2022-06-19 22:37:55 -0400

    update

--- other/github_projects_cloning/dummy.sh
+++ other/github_projects_cloning/dummy.sh
@@ -2,7 +2,6 @@ cd /home/leo_zhang/Documents/GitHub && \
 git clone git@github.com:leozhang1/AmazonPriceTrackerTool.git && \
 git clone git@github.com:leozhang1/ARHW1.git && \
 git clone git@github.com:leozhang1/bmbl_bot.git && \
-git clone git@github.com:leozhang1/tdr_bot.git && \
 git clone git@github.com:leozhang1/DroneFun.git && \
 git clone git@github.com:leozhang1/easy-apply-linkedin.git  && \
 git clone git@github.com:leozhang1/FileManagementTool.git && \
@@ -10,10 +9,11 @@ git clone git@github.com:leozhang1/FlightScraper.git && \
 git clone git@github.com:leozhang1/GeneralDB.git && \
 git clone git@github.com:leozhang1/GetDailyPic.git && \
 git clone git@github.com:leozhang1/GithubAutomation.git  && \
+git clone git@github.com:leozhang1/important_docs.git  && \
+git clone git@github.com:leozhang1/Journal.git  && \
 git clone git@github.com:leozhang1/LeoZhang.git && \
+git clone git@github.com:leozhang1/music_collection.git  && \
 # git clone git@github.com:leozhang1/my_linux_configs.git && \
-git clone git@github.com:leozhang1/Journal.git && \
-git clone git@github.com:leozhang1/nasa_daily_pic_collection.git  && \
 git clone git@github.com:leozhang1/NewsScraper.git && \
 git clone git@github.com:leozhang1/okcupidbot.git && \
 git clone git@github.com:leozhang1/PasswordManagerNodeJs.git && \
@@ -21,6 +21,7 @@ git clone git@github.com:leozhang1/PersonalPortfolioFlutterWeb.git && \
 git clone git@github.com:leozhang1/PongGame.git && \
 git clone git@github.com:leozhang1/practice_interview_problems.git && \
 git clone git@github.com:leozhang1/summer_2022_class.git && \
+git clone git@github.com:leozhang1/TaxLienScraper.git && \
 git clone git@github.com:leozhang1/tdr_bot.git && \
 git clone git@github.com:leozhang1/TruistCreditCardManager.git && \
 git clone git@github.com:leozhang1/Zoom-Automation-Python.git
