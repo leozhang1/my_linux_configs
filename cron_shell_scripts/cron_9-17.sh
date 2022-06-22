36875bf70fc39a5bf2c3153909f80a35aa1dd3e9
Leo Zhang <leotheasianlion@gmail.com>
2022-06-22 18:34:06 -0400

    update

--- cron_shell_scripts/cron_9-17.sh
+++ cron_shell_scripts/cron_9-17.sh
@@ -2,7 +2,7 @@
 python_abs_path='/home/leo_zhang/.conda/envs/web_auto/bin/python'
 $python_abs_path /home/leo_zhang/Documents/GitHub/AmazonPriceTrackerTool/AmazonScraper.py
 $python_abs_path /home/leo_zhang/Documents/GitHub/NewsScraper/NewsScraper.py
-$python_abs_path /home/leo_zhang/Documents/GitHub/GetDailyPic/get_daily_pic.py
+export DISPLAY=:0 && $python_abs_path /home/leo_zhang/Documents/GitHub/GetDailyPic/get_daily_pic.py
 # export DISPLAY=:0 && $python_abs_path /home/leo_zhang/Documents/GitHub/FlightScraper/FlightScraper.py
 # end cron_9-17.sh
 
---
+++ python_configs/paths.sh
@@ -0,0 +1,5 @@
+
+web_auto_path='/home/leo_zhang/.conda/envs/web_auto/bin/python'
+time_series_path='/home/leo_zhang/.conda/envs/time_series/bin/python'
+
+
